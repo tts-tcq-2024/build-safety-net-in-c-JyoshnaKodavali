0cee10cfcc3959f9b6af9a1c0ef8ae77dfdd4504
JyoshnaKodavali <73461390+JyoshnaKodavali@users.noreply.github.com>
2024-07-17 09:49:05 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -5,37 +5,124 @@
 #include <ctype.h>
 #include <string.h>
 
-char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
-    }
-}
+int LetterList(char letter, int row_value ,int column_value)
+{
+    char SetArray[6][8] = {
+    {'B','F','P','V'},
+    {'C','G','J','K','Q','S','X','Z'},
+    {'D','T'},
+    {'L'},
+    {'M','N'},
+    {'R'}
+    };
 
-void generateSoundex(const char *name, char *soundex) {
-    int len = strlen(name);
-    soundex[0] = toupper(name[0]);
-    int sIndex = 1;
-
-    for (int i = 1; i < len && sIndex < 4; i++) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != soundex[sIndex - 1]) {
-            soundex[sIndex++] = code;
+     for (int i = 0; i < column_value; i++)
+    {
+        if (letter == SetArray[row_value][i])
+        {
+            return 1;
+            break;
         }
     }
-
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';
-    }
-
-    soundex[4] = '\0';
-    
+    return 0;
 }
 
+
+char ValueCheck6(char c)
+{
+    int row_value = 5;
+    int column_value = 1;
+    char letter = toupper(c);
+    if (LetterList(letter,row_value,column_value) == 1)
+    {
+        return '6';
+    }
+    else
+    {
+        return '0';
+    }
+}
+
+
+char ValueCheck5(char c)
+{
+    int row_value = 4;
+    int column_value = 2;
+    char letter = toupper(c);
+    if (LetterList(letter,row_value,column_value) == 1)
+    {
+        return '5';
+    }
+    else
+    {
+        return ValueCheck6(c);
+    }
+}
+
+
+char ValueCheck4(char c)
+{
+    int row_value = 3;
+    int column_value = 1;
+    char letter = toupper(c);
+    if (LetterList(letter,row_value,column_value) == 1)
+    {
+        return '4';
+    }
+    else
+    {
+        return ValueCheck5(c);
+    }
+}
+
+
+char ValueCheck3(char c)
+{
+    int row_value = 2;
+    int column_value = 2;
+    char letter = toupper(c);
+    if (LetterList(letter,row_value,column_value) == 1)
+    {
+        return '3';
+    }
+    else
+    {
+        return ValueCheck4(c);
+    }
+}
+char ValueCheck2(char c)
+{
+    int row_value = 1;
+    int column_value = 8;
+    char letter = toupper(c);
+    if (LetterList(letter,row_value,column_value) == 1)
+    {
+        return '2';
+    }
+    else
+    {
+        return ValueCheck3(c);
+    }
+}
+
+
+char ValueCheck1(char c)
+{
+    int row_value = 0;
+    int column_value = 4;
+    char letter = toupper(c);
+    if (LetterList(letter,row_value,column_value) == 1)
+    {
+        return '1';
+    }
+    else
+    {
+        return ValueCheck2(c);
+    }
+}
+
+char getSoundexCode(char c){ 
+    return ValueCheck1(c);
+
+}
 #endif // SOUNDEX_H
